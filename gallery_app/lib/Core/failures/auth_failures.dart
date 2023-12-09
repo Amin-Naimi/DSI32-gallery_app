0880d1faa390a98463d2c4ea402cb871d16075ba
Mohamed <ahnbi32@gmail.com>
2023-12-09 18:27:28 +0100

    First commit

---
+++ gallery_app/lib/Core/failures/auth_failures.dart
@@ -0,0 +1,36 @@
+import 'failures.dart';
+
+class LoginUserFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class RegisterUserFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class RegisterUserWeakPwdFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class RegisterUserUsedEmailFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class SignInUserNotFoundFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class SignInWrongPwdFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
+
+class SignInFailure extends Failure {
+  @override
+  List<Object?> get props => [];
+}
---
+++ gallery_app/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gallery_app/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("initialization firebase terminée");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello Flutter"),
+      ),
+    );
+  }
+}
